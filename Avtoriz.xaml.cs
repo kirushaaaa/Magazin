014ceeb6fe2895baf66c116cca9c98ae9956ea42
Name1 <nchernyaev06@gmail.com>
2024-02-17 15:00:51 +0400

    initial commit

---
+++ Avtoriz.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Praktika1
+{
+    /// <summary>
+    /// Логика взаимодействия для Avtoriz.xaml
+    /// </summary>
+    public partial class Avtoriz : Window
+    {
+        public Avtoriz()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Registr rg = new Registr();
+            rg.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Lk lk = new Lk();
+            lk.Show();
+            this.Close();
+        }
+    }
+}
